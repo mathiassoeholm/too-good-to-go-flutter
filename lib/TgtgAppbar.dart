c405d8dbcce35185adf30f0cbdeb46abfd38a602
Mathias Søholm <mathias.soholm@consultant.lego.com>
2018-12-14 22:17:18 +0100

    Finished design of AppBar

--- lib/TgtgAppbar.dart
+++ lib/TgtgAppbar.dart
@@ -6,19 +6,40 @@ class TGTGAppBar extends StatelessWidget {
   Widget build(BuildContext context) => Container(
         height: 50.0 + MediaQuery.of(context).padding.top,
         decoration: BoxDecoration(color: Theme.of(context).primaryColor),
-        padding: EdgeInsets.only(bottom: 8),
         child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           crossAxisAlignment: CrossAxisAlignment.end,
           children: <Widget>[
-            Text(
-              'Too Good To Go',
-              style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.w800,
-                  fontSize: AppTheme.titleFontSize),
+            createIconButton(Icons.menu,
+              context: context,
+              padding: EdgeInsets.only(left: 8),
+              onPressed: null),
+            Container(
+              padding: EdgeInsets.only(bottom: 12),
+              child: Text('Too Good To Go',
+                style: TextStyle(
+                    color: Theme.of(context).primaryTextTheme.title.color,
+                    fontWeight: FontWeight.w800,
+                    fontSize: AppTheme.titleFontSize),
+              ),
             ),
+            createIconButton(Icons.inbox,
+              context: context,
+              padding: EdgeInsets.only(right: 8),
+              onPressed: null),
           ],
         ),
+      );
+
+    IconButton createIconButton(IconData iconData, {
+      BuildContext context,
+      EdgeInsets padding,
+      VoidCallback onPressed
+    }) => IconButton(
+        padding: padding,
+        icon: Icon(iconData, 
+          size: 35,
+          color: Theme.of(context).primaryTextTheme.title.color),
+        onPressed: onPressed,
       );
 }
--- lib/theme.dart
+++ lib/theme.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 
 ThemeData getTheme() => ThemeData(
-  primaryColor: Color.fromARGB(255, 126, 189, 181),
+  primaryColor: Colors.teal[200],
+  primaryTextTheme: TextTheme(
+    title: TextStyle(
+      color: Colors.white
+    )
+  )
 );
 
 class AppTheme {
   static const appBarElevation = 0.0;
-  static const titleFontSize = 18.0;
+  static const titleFontSize = 19.0;
 }
