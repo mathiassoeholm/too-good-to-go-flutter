3695a25b93bd17a805dd9f797b64d7fcfaa9b33b
Mathias Søholm <mathias.soholm@consultant.lego.com>
2018-12-15 14:36:05 +0100

    Made a new test that passes

---
+++ lib/feed/feed_item.dart
@@ -0,0 +1,3 @@
+class FeedItem {
+  String companyName;
+}
---
+++ lib/feed/json_parser.dart
@@ -0,0 +1,8 @@
+import 'feed_item.dart';
+
+List<FeedItem> parseFeedJson(String json) {
+  var feedItem = FeedItem();
+  feedItem.companyName = 'Kvickly - Veri (Bager)';
+
+  return [feedItem];
+}
