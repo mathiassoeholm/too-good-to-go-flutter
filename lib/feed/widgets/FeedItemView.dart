18b43390a75f773227ef010cfac1751bea9092ba
Mathias <mathiassoeholm@gmail.com>
2018-12-22 23:34:56 +0100

    Started work on FeedItemView

--- lib/feed/widgets/FeedItemView.dart
+++ lib/feed/widgets/FeedItemView.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:too_good_to_go/feed/models/feed_item.dart';
 
 class FeedItemView extends StatelessWidget {
+  static const blackBarHeight = 25.0;
+
   final FeedItem item;
   
   const FeedItemView(
@@ -11,17 +13,49 @@ class FeedItemView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(8.0),
+      padding: const EdgeInsets.fromLTRB(12, 5, 12, 5),
       child: Card(
-        child: Column(
+        child: Stack(
+          fit: StackFit.loose,
           children: <Widget>[
-            Image.network(
-              item.coverImage,
-              height: 120,
+            Image.network(item.coverImage,
               width: double.infinity,
+              height: 140,
               fit: BoxFit.cover,
             ),
-            Text(item.companyName),
+            Positioned(
+              bottom: 0,
+              height: blackBarHeight,
+              left: 0,
+              right: 0,
+              child: DecoratedBox(
+                decoration: BoxDecoration(
+                  color: Colors.black.withAlpha(150),
+                ),
+              ),
+            ),
+            Positioned(
+              bottom: 0,
+              left: 5,
+              height: blackBarHeight,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  CircleAvatar(
+                    backgroundColor: Colors.green,
+                    radius: 9,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 8.0),
+                    child: Text('${item.itemsLeft} left',
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            )
           ],
         ),
       ),
