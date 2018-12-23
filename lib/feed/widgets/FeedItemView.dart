b78c3058e80c670488704ba69e446808515de0d2
Mathias <mathiassoeholm@gmail.com>
2018-12-23 10:36:34 +0100

    Refactored FeedItemView

--- lib/feed/widgets/FeedItemView.dart
+++ lib/feed/widgets/FeedItemView.dart
@@ -5,11 +5,11 @@ class FeedItemView extends StatelessWidget {
   static const blackBarHeight = 25.0;
 
   final FeedItem item;
-  
+
   const FeedItemView(
     this.item,
   );
-  
+
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -17,48 +17,57 @@ class FeedItemView extends StatelessWidget {
       child: Card(
         child: Stack(
           fit: StackFit.loose,
-          children: <Widget>[
-            Image.network(item.coverImage,
-              width: double.infinity,
-              height: 140,
-              fit: BoxFit.cover,
-            ),
-            Positioned(
-              bottom: 0,
-              height: blackBarHeight,
-              left: 0,
-              right: 0,
-              child: DecoratedBox(
-                decoration: BoxDecoration(
-                  color: Colors.black.withAlpha(150),
-                ),
-              ),
-            ),
-            Positioned(
-              bottom: 0,
-              left: 5,
-              height: blackBarHeight,
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  CircleAvatar(
-                    backgroundColor: Colors.green,
-                    radius: 9,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 8.0),
-                    child: Text('${item.itemsLeft} left',
-                      style: TextStyle(
-                        color: Colors.white,
-                      ),
-                    ),
-                  )
-                ],
-              ),
-            )
-          ],
+          children:
+            _buildImage()
+            ..addAll(_buildBlackBar()),
         ),
       ),
     );
   }
+
+  List<Widget> _buildImage() => [
+    Image.network(
+      item.coverImage,
+      width: double.infinity,
+      height: 140,
+      fit: BoxFit.cover,
+    )
+  ];
+
+  List<Widget> _buildBlackBar() => [
+        Positioned(
+          bottom: 0,
+          height: blackBarHeight,
+          left: 0,
+          right: 0,
+          child: DecoratedBox(
+            decoration: BoxDecoration(
+              color: Colors.black.withAlpha(150),
+            ),
+          ),
+        ),
+        Positioned(
+          bottom: 0,
+          left: 5,
+          height: blackBarHeight,
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                backgroundColor: Colors.green,
+                radius: 9,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 8.0),
+                child: Text(
+                  '${item.itemsLeft} left',
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+              )
+            ],
+          ),
+        )
+      ];
 }
